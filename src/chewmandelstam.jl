aafa08ca5fc1b9053d75d1c6c95d46f8915f93bc
Misha Mikhasenko <mikhail.mikhasenko@gmail.com>
2020-11-18 13:34:02 +0100

    Omega BW

--- src/chewmandelstam.jl
+++ src/chewmandelstam.jl
@@ -2,9 +2,7 @@
 k(s) = sqrt(s/4-mπ^2)
 CM_Pwave(s; R=5) = s/π * quadgk(s′->k(s′)^3/ (1+(R*k(s′))^2) / sqrt(s′) / (s′*(s′-s-1e-7im)), 4mπ^2, Inf)[1]
 
-function BW_Pwave(s; R=0.5, endep=:CM)
-    # mρ = 0.77526
-    Γρ = 0.1478
+function BW_Pwave(s; R=Nominal.R, endep=:CM)
     Γ = Γρ;
     if endep == :rho
         p = k(s); p0 = k(mρ^2)
@@ -21,3 +19,5 @@ function phiBW_Pwave(s; R=0.5, endep=:CM)
     a = BW_Pwave(s; R=R, endep=endep)
     return atan(imag(a), real(a))
 end
+
+I_ωBW(s) = abs2(1/(mω^2-s-1im*mω*Γω))
